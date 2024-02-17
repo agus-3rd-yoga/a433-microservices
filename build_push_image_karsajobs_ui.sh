4fb157949cf844ea41124060d1c7381753534047
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2024-02-18 01:46:16 +0700

    Fix script jobs

--- build_push_image_karsajobs_ui.sh
+++ build_push_image_karsajobs_ui.sh
@@ -10,7 +10,7 @@ clear
 build () {
 echo "Build image from Dockerfile"
 echo -e "----------------------------\n\n"
-docker build --tag karsajobs-ui:latest -f /home/sysadmin/backend/Dockerfile .
+docker build --tag karsajobs-ui:latest -f /home/sysadmin/frontend/Dockerfile .
 }
 
 ## Create function list to listing available local image
