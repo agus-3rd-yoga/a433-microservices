d898db8ae4ba65641409058f0df0e9b2712f3cd1
agus-3rd-yoga <agus.3rd.yoga@gmail.com>
2024-02-17 15:38:07 +0700

    Change from podman to docker

--- build_push_image.sh
+++ build_push_image.sh
@@ -11,7 +11,7 @@ build () {
 echo "Build image from Dockerfile"
 echo "---------------------------"
 echo ""
-podman build --tag item-app:v1 -f /home/sysadmin/a433-microservices/Dockerfile
+docker build --tag item-app:v1 -f /home/sysadmin/a433-microservices/Dockerfile
 }
 
 ## Create function list to listing available local image
@@ -19,7 +19,7 @@ list () {
 echo "List images on local registry"
 echo "-----------------------------"
 echo ""
-podman image ls
+docker image ls
 }
 
 ## Create function name to rename or formatting image label/tag to match Dockerhub convention naming/tagging
@@ -27,8 +27,8 @@ name () {
 echo "Change image name to match DockerHub naming format"
 echo "--------------------------------------------------"
 echo ""
-podman tag $(podman image ls | grep -E 'item-app|v1' | awk '{print $3}'|tail -1) agus3rdyoga/item-app:v1
-podman image ls
+docker tag $(docker image ls | grep -E 'item-app|v1' | awk '{print $3}'|tail -1) agus3rdyoga/item-app:v1
+docker image ls
 }
 
 ## Create function login to login to dockerhub using provided token stored on ~/.bashrc & exported as $TOKEN_DOCKER_HUB variable
@@ -36,7 +36,7 @@ login () {
 echo "Login to docker hub"
 echo "-------------------"
 echo ""
-echo $TOKEN_DOCKER_HUB | podman login -v docker.io -u agus3rdyoga --password-stdin 
+echo $TOKEN_DOCKER_HUB | docker login -v docker.io -u agus3rdyoga --password-stdin 
 }
 
 ## Create function upload to upload final image to dockerhub 
@@ -44,7 +44,7 @@ upload () {
 echo "Upload image to docker hub"
 echo "--------------------------"
 echo ""
-podman push $(podman image ls | grep -E 'item-app|v1' | awk '{print $3}'|tail -1) docker://docker.io/agus3rdyoga/item-app:v1
+docker push $(docker image ls | grep -E 'item-app|v1' | awk '{print $3}'|tail -1) docker://docker.io/agus3rdyoga/item-app:v1
 }
 
 ## Run all function in sequence following the rules from Dicoding material 
